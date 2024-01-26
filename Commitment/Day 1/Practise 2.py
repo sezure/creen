5274953f8c8b5a4251bcda07aee0f3a6e8bac4a3
2408081730@qq.com <142392384+sezure@users.noreply.github.com>
2024-01-27 00:34:01 +0800

    Practise1.2.3，2的内容不会

---
+++ Commitment/Day 1/Practise 2.py
@@ -0,0 +1,4 @@
+print('what\'s your name')
+name=input()
+print("hello "" name "" would you like to learn some Python today?")#name那块地方出错了
+print("Albert Einstein once said, “A person who never made a mistake never tried anything new.”")
---
+++ Commitment/Day 1/Practise 3.py
@@ -0,0 +1,4 @@
+print(5+3)
+print(10-2)
+print(8//1)
+print(8**1)
